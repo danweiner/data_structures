a153158def92ffdb3b21998e7338ab4d59b5bc9f
Dan Weiner <daweiner16@gmail.com>
2016-05-16 16:54:52 -0400

    add initial code for stack

--- Starters PA1 2/check_brackets_in_code/check_brackets.py
+++ Starters PA1 2/check_brackets_in_code/check_brackets.py
@@ -17,16 +17,24 @@ class Bracket:
         return False
 
 if __name__ == "__main__":
-    text = sys.stdin.read()
+    text = '()))'#sys.stdin.read()
 
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next == '(' or next == '[' or next == '{':
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(text[i])
             pass
 
         if next == ')' or next == ']' or next == '}':
             # Process closing bracket, write your code here
+            if not opening_brackets_stack:
+                break
+            opening_brackets_stack.pop()
             pass
 
+    print(opening_brackets_stack)
+          
     # Printing answer, write your code here
+    if not opening_brackets_stack:
+        print('Success')
